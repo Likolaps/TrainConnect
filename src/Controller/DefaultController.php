0c8a0d6f37cb802a808ae43335bd6c9ccb3c52ce
Natan Brault <natan.brault@laposte.net>
2024-09-25 10:47:53 +0200

    added default controller and index

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\LineRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class DefaultController extends AbstractController
+{
+    #[Route('/', name: 'app_index')]
+    public function index(LineRepository $lineRepository): Response
+    {
+        $lines = $lineRepository->findAll();
+
+
+        return $this->render('default/index.html.twig', [
+            'lines' => $lines,
+        ]);
+    }
+}
