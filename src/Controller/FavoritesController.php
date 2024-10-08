9ddca12b960b8c2b36da9083fd375bcc922e3302
nee-corn <61169975+nee-corn@users.noreply.github.com>
2024-10-08 10:15:48 +0200

    delete button work plus quality code on index

--- src/Controller/FavoritesController.php
+++ src/Controller/FavoritesController.php
@@ -8,6 +8,7 @@ use App\Repository\FavoritesRepository;
 use App\Repository\LineRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -15,27 +16,25 @@ class FavoritesController extends AbstractController
 {
     #[Route('/favorites', name: 'app_favorites')]
     public function index(FavoritesRepository $allFavs, EntityManagerInterface $entityManager): Response
-    {   
-        $favs=$allFavs->findAll();
-        $lines=[];
-        foreach($favs as $fav) 
-        {
-            array_push($lines , $fav->getLine());
+    {
+        $favs = $allFavs->findAll();
+        $lines = [];
+        foreach ($favs as $fav) {
+            array_push($lines, $fav->getLine());
         }
 
         return $this->render('favorites/index.html.twig', [
-            'lines' => $lines,
+            "favs" => $favs
         ]);
     }
 
     #[Route('/favorites/add/{id}', name: 'app_add_to_favorites')]
-    public function addToFav(LineRepository $lines, $id, EntityManagerInterface $entityManager,FavoritesRepository $favoritesRepository): Response
+    public function addToFav(LineRepository $lines, $id, EntityManagerInterface $entityManager, FavoritesRepository $favoritesRepository): Response
     {
-        
-        $line= $lines->find($id);
-        $user=$this->getUser();
-        if($favoritesRepository->findOneBy(array('line'=>$id,'user'=>$user->getId())) == [])
-        {
+
+        $line = $lines->find($id);
+        $user = $this->getUser();
+        if ($favoritesRepository->findOneBy(array('line' => $id, 'user' => $user->getId())) == []) {
             $fav = new Favorites();
             $fav->setLine($line);
             $fav->setUser($user);
@@ -46,5 +45,24 @@ class FavoritesController extends AbstractController
 
         return $this->redirectToRoute('app_favorites');
     }
-}
 
+    #[Route('/{id}', name: 'app_fav_delete')]
+    public function delete($id, EntityManagerInterface $entityManager, FavoritesRepository $favoritesRepository): Response
+    {
+        $user = $this->getUser();
+        $fav = $favoritesRepository->find($id);
+        $entityManager->remove($fav);
+        $entityManager->flush();
+
+
+
+
+
+        // if ($this->isCsrfTokenValid('delete' . $fav->getId(), $request->getPayload()->getString('_token'))) {
+        //     $entityManager->remove($fav);
+        //     $entityManager->flush();
+        // }
+
+        return $this->redirectToRoute('app_favorites', [], Response::HTTP_SEE_OTHER);
+    }
+}
